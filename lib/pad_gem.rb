427800699599a96c901fb9614d0bee511a8e3015
Nico Schuele -mini <nicoschuele@gmail.com>
2016-02-21 17:17:56 +0100

    Define high level workflow

--- lib/pad_gem.rb
+++ lib/pad_gem.rb
@@ -1,7 +1,20 @@
 require_relative "pad_gem/version"
+require_relative "pad_gem/menu"
 
 module PadGem
-  def self.main(args)
-    puts args
+
+  def self.main(arg)
+    # TODO: implement a no option that displays possible options
+
+    # TODO: implement a -n (--new) option
+      # TODO: build options hash from a cli menu
+      # TODO: call self.create(options)
+
+    # TODO: implement a -v (--version) option
   end
+
+  def self.create(options)
+    # TODO: implement creation of the gem based on options hash
+  end
+
 end
---
+++ lib/pad_gem/menu.rb
@@ -0,0 +1,17 @@
+module PadGem
+  module Menu
+
+    def self.ask
+      # TODO: implements menu, returning a hash, asking for the following questions:
+      # PADGEM_GEM_NAME
+      # PADGEM_AUTHOR
+      # PADGEM_YEAR
+      # PADGEM_GEM_RUBY_NAME <-- computed from PadUtils.convert_to_ruby_name
+      # PADGEM_MIN_RUBY_VERSION
+      # PADGEM_EMAIL
+      # PADGEM_HOMEPAGE
+      # PADGEM_EXECUTABLE
+    end
+
+  end
+end
