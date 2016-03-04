bee2d5cd2a2bf507bb6d411d3a7f12c08b5795b1
Nico Schuele -mini <nicoschuele@gmail.com>
2016-03-04 01:59:51 +0100

    Add custom test framework

---
+++ test/test_runner.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'pad_utils'
+require 'pad_gem'
+
+start_time = Time.now
+number_of_tests = 0
+errors_list = []
+
+puts
+
+PadUtils.puts_c "Running tests...", :blue
+
+Dir["units/*_test.rb"].each do |file|
+  require_relative file
+
+  class_name = PadUtils.filename_to_classname(file)
+
+  clazz = PadUtils.filename_to_class(file)
+  c = clazz.new(class_name)
+  errors = c.run
+
+  if errors[:errors] > 0
+    errors_list << errors
+  end
+
+  number_of_tests += 1
+end
+
+end_time = Time.now
+interval = PadUtils.interval start_time, end_time, :seconds
+
+PadUtils.puts_c "Finished running #{number_of_tests} tests in #{interval} seconds", :blue
+if errors_list.length > 0
+  PadUtils.puts_c "--> Failed (#{errors_list.length}): ", :error
+  errors_list.each do |err|
+    PadUtils.puts_c "- #{err[:name]}: #{err[:errors]} error(s)"
+  end
+else
+  PadUtils.puts_c "--> 0 errors", :green
+end
+puts
